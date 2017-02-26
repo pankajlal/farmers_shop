02ed04d653f9b9a83959a1a5f19ef3f92ed8bf79
Pankaj Ratan Lal <pankaj.r.lal@gmail.com>
2017-02-26 19:08:32 +0530

    updated for gulp related stuff

---
+++ frontend/gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pankaj on 26/2/17.
+ */
+var gulp = require('gulp');
+var gulp_util = require('gulp-util');
+
+gulp.task('logger', function(){
+    console.log("Hello Something");
+});
