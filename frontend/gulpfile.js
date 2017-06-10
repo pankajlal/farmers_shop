d5cf1d47d0859376c2d74046071e41de9a7c9a20
Pankaj Ratan Lal <pankaj.r.lal@gmail.com>
2017-06-10 08:01:36 +0530

    beginning to work now

--- frontend/gulpfile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by pankaj on 26/2/17.
- */
-var gulp = require('gulp');
-var gulp_util = require('gulp-util');
-
-gulp.task('logger', function(){
-    console.log("Hello Something");
-});
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app/base.html'
+})
+
+export class AppComponent {
+    title = 'Farmers Shop';
+
+}
+
+
